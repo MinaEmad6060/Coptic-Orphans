1697102b74f94d49c64fb96de30250e2a0f502c8
Mina Emad <minaemad6060@gmail.com>
2025-03-14 16:58:58 +0200

    Initial Commit

---
+++ Coptic Orphans/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coptic Orphans
+//
+//  Created by Mina Emad on 14/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
